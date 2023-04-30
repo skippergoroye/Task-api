a992e4d7d138da686957593b2ffc4b71a7ad6c6a
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-04-30 15:10:58 +0100

    mapping over an api

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
+
+
+function App() {
+  const [data, setData] = useState([])
+
+  // useEffect(() => {
+  //   fetch('https://gorest.co.in/public/v2/users')
+  //     .then(response => response.json())
+  //     .then(data => setData(data))
+
+  //     console.log(data)
+  // }, [])
+
+  useEffect(() => {
+       fetch('https://gorest.co.in/public/v2/users')
+          .then(response => response.json())
+          .then(data => setData(data))
+      console.log(data)
+  },[])
+
+ 
+
+  return (
+    <>
+     <div>
+       <h1>hello</h1>
+       <ul>
+        {data && data.map(({ id, name, email, gender, status, title }) => (
+            <li key={id}>
+              <h2>Name: {name}</h2>
+              <h5>Email: {email}</h5>
+              <h5>Gender: {gender}</h5>
+              <h5>Status: {status}</h5>
+            </li>
+        ))}
+      </ul>
+     </div>
+    </>
+  )
+}
+
+export default App
